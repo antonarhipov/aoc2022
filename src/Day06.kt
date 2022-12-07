e19ce3ef4c562f15709e38a62e23c183c02b9b63
anton <ant.arhipov@gmail.com>
2022-12-07 08:38:24 +0200

    a few changes

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,7 +1,8 @@
 fun main() {
     fun CharSequence.allUnique() = toSet().count() == length
 
-    fun solve(input: String, windowSize: Int) = input.windowed(windowSize) { it.allUnique() }.indexOf(true) + windowSize
+    fun solve(input: String, windowSize: Int) =
+        input.windowed(windowSize).indexOfFirst  { it.allUnique() } + windowSize
 
     fun solveNaively(input: String, windowSize: Int): Int {
         val windowed = input.windowed(windowSize)
